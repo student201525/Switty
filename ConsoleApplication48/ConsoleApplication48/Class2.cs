6ae209e336a2e1a119350f2eb2aeb569f73873f6
student201525 <okynev.nikita@yandex.ru>
2017-02-17 08:52:26 +0500

    MyFirstGit

---
+++ ConsoleApplication48/ConsoleApplication48/Class2.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication47
+{
+    class Points
+    {
+        public readonly double x, y;
+        public Points(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Edge
+    {
+        public Points v1;
+        public Points v2;
+        public double lenght;
+        public Edge(Points v1, Points v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            lenght = Math.Sqrt(Math.Pow(v2.x - v1.x, 2) + Math.Pow(v2.y - v1.y, 2));
+        }
+
+    }
+
+    class Triangle
+    {
+
+        public double a;
+        Points p1;
+        Points p2;
+        Points p3;
+        Edge AB;
+        Edge BC;
+        Edge CA;
+        public Triangle(Points p1, Points p2, Points p3)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+
+            AB = new Edge(p1, p2);
+            BC = new Edge(p2, p3);
+            CA = new Edge(p3, p1);
+
+
+        }
+
+        public bool CheckReal()//Существование 
+        {
+
+            if (AB.lenght + BC.lenght <= CA.lenght || AB.lenght + CA.lenght <= BC.lenght || BC.lenght + CA.lenght <= AB.lenght)
+            {
+                Console.WriteLine("Треугольник не существует");
+              // Environment.Exit(0);
+                return false;
+            }
+            return true;
+        }
+
+        public double GetPerimetre()//Периметр 
+        {
+          
+                double primente = AB.lenght + BC.lenght + CA.lenght;
+                a = primente;
+               return primente;
+            
+        }
+
+        public double GetArea()//Площадь 
+        {
+                double p = a / 2;
+                double area = Math.Sqrt(p * (p - AB.lenght) * (p - BC.lenght) * (p - CA.lenght));//gerona 
+                return area;
+        }
+
+        public bool CheckIsosceles()//Равнобедренность 
+        {
+
+            if (AB.lenght == BC.lenght || AB.lenght == CA.lenght || BC.lenght == CA.lenght)
+            {
+                Console.WriteLine("Треугольник равнобедренный");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не равнобедренный");
+                return false;
+            }
+        }
+
+        public bool CheckRight()
+        {
+            if ((Math.Round(GetArea(), 2) == Math.Round(AB.lenght * BC.lenght / 2, 2))
+            || (Math.Round(GetArea(), 2) == Math.Round(CA.lenght * BC.lenght / 2, 2))
+            || (Math.Round(GetArea(), 2) == Math.Round(AB.lenght * CA.lenght / 2, 2)))
+
+            {
+                Console.WriteLine("Этот треугольник прямоугольный");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Этот треугольник не прямоугольный");
+                return false;
+            }
+        }
+
+        
+        //public bool CheckRight()//Прямоугольность 
+        //{
+        //    if (Math.Pow(AB.lenght, 2) + Math.Pow(BC.lenght, 2) == Math.Pow(CA.lenght, 2)
+        //        || Math.Pow(AB.lenght, 2) + Math.Pow(CA.lenght, 2) == Math.Pow(BC.lenght, 2) 
+        //        || Math.Pow(CA.lenght, 2) + Math.Pow(BC.lenght, 2) == Math.Pow(AB.lenght, 2))
+        //    {
+        //        Console.WriteLine("Этот треугольник прямоугольный");
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Этот треугольник не прямоугольный");
+        //        return false;
+        //    }
+        //}
+    }
+}
---
+++ ConsoleApplication48/ConsoleApplication48/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication47
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {        
+
+            Triangle[] ArrayOfTriangles = new Triangle[6];
+
+          var p1 = new Points(0, 0);
+          var p2 = new Points(0, 0);
+          var p3 = new Points(0, 0);
+
+            double SumPerimetre = 0;
+            double CounterPerimetre = 0;
+            double SumArea = 0;
+            double CounterArea = 0;
+
+                double[] x1 = { 5, 0, 1, 6, 2, 0 };
+                double[] y1 = { 3, 0, 2, 5, 4, 0 };
+
+                double[] x2 = { 2, 1, 3, 4, 6, 1 };
+                double[] y2 = { 2, 0, 4, 3, 8, 0 };
+
+                double[] x3 = { 5, 0, 5, 2, 10, 0 };
+                double[] y3 = { 2, 1, 6, 1, 12, 1 };  
+
+            for (int i = 0; i < 6; i++)
+            {
+
+                ArrayOfTriangles[i] = new Triangle(
+                p1 = new Points(x1[i], y1[i]),
+                p2 = new Points(x2[i], y2[i]),
+                p3 = new Points(x3[i], y3[i]));
+
+
+            }
+
+            for (int i = 0; i < ArrayOfTriangles.Length; i++)
+            {
+                Console.WriteLine("*************************************");
+                Console.WriteLine("Треугольник - {0}", i + 1);
+
+                if (ArrayOfTriangles[i].CheckReal())
+                {
+
+                    Console.WriteLine("Периметр = " + ArrayOfTriangles[i].GetPerimetre());
+                    Console.WriteLine("Площадь = " + ArrayOfTriangles[i].GetArea());
+
+                    if (ArrayOfTriangles[i].CheckIsosceles())
+                    {
+                        SumArea += ArrayOfTriangles[i].GetArea();
+                        CounterArea++;
+                    }
+
+                    if (ArrayOfTriangles[i].CheckRight())
+                    {
+                        SumPerimetre += ArrayOfTriangles[i].GetPerimetre();
+                        CounterPerimetre++;
+                    }
+                }
+            }
+
+            Console.WriteLine("*************************************");
+            Console.WriteLine("*************************************");
+            Console.WriteLine("*************************************");
+            Console.WriteLine("Средний периметр = " + SumPerimetre / CounterPerimetre);
+            Console.WriteLine("Средняя площадь = " + SumArea / CounterArea);
+
+            }
+    }
+}
